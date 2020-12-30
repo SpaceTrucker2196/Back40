5a0b19e4fa7a952ead82c9af5ece65e7037a8386
Jeff Kunzelman <jeff@river.io>
2020-12-30 10:23:13 -0700

    db config changes

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -11,18 +11,15 @@ public func configure(_ app: Application) throws {
 //    let postgreSQL = PostgreSQLDatabase(config: postgreSQLConfig)
     
 
-   // try app.services.register(PostgreSQLProvider())
-    
-    let configuration = PostgresConfiguration(
-        hostname: "localhost",
-        username: "back40",
-        password: "vapor_password",
-        database: "back40"
-    )
-    
 
+    if let url = Environment.get("DATABASE_URL") {
+      // configuring database
+        try app.databases.use(.postgres(url:url), as: .psql)
+    } else {
+      // ...
+    }
     
-    try app.databases.use(.postgres(url: "postgres://sfmvmuuhosqowj:7a887858d12b7ceb6197db2fbde325bfdb0ed0a2cebfff724bc9e4af84e655b9@ec2-54-175-243-75.compute-1.amazonaws.com:5432/d42pnf4pp733t1"), as: .psql)
+  
     
     
     app.migrations.add(CreateUsers())
